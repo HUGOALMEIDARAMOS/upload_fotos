7a2fabb98e16c2b8c948eb0d19d03cafcafaac49
HUGOALMEIDARAMOS <hugo.undb@gmail.com>
2016-06-20 12:50:53 -0300

    Update adm_fotos.php

--- adm_fotos.php
+++ adm_fotos.php
@@ -1,5 +1,18 @@
 <?php
+
+require_once 'conexao.php';
+
 $id = $_GET['id_galeria'];
+
+
+
+$query = "SELECT *  FROM fotos WHERE  fkgaleria='$id';";
+$p_sql = $pdo->prepare($query);
+$p_sql->execute();
+$dados_fotos = $p_sql->fetchAll(PDO::FETCH_ASSOC);
+
+
+
 ?>
 
 <!DOCTYPE html>
@@ -9,9 +22,7 @@ $id = $_GET['id_galeria'];
     <title>Galeria de Fotos</title>
 
     <style>
-        label, input {
-            display: block;
-        }
+
     </style>
 
 </head>
@@ -22,12 +33,85 @@ $id = $_GET['id_galeria'];
 
 <form method="post" enctype="multipart/form-data" action="enviar_fotos.php">
 
-    <input type="hidden" name="idgaleria" value="<?php echo $id; ?>"/>
-    <label>Titulo</label> <input type="text" name="titulo"/>
-    <label>Arquivo</label><input type="file" name="arquivo"/>
-    <input type="submit" value="adcionar fotos"/>
+
+     <div id="input_files">
+           <div>
+                <input type="hidden" name="idgaleria" value="<?php echo $id; ?>"/>
+                <label>Titulo</label>
+                <input type="text" name="titulo[]"/>
+                <label>Arquivo</label>
+               <input type="file" name="arquivo[]"/>
+           </div>
+     </div>
+
+    <input type="submit" value="adcionar fotos" name="upload"/>
+    <input type="button" id="outra_foto" value="+ foto"/>
+
+
+    <script type="text/javascript">
+            var boton=document.getElementById('outra_foto');
+                boton.onclick=function () {
+
+                    var div_cont=document.createElement('div');
+                     var label1=document.createElement('label1');
+                          label1.innerHTML='Titulo';
+                     var input1=document.createElement('input');
+                         input1.type='text';
+                         input1.name='titulo[]';
+
+
+                     var label2=document.createElement('label2');
+                         label2.innerHTML='Arquivo';
+                     var input2=document.createElement('input');
+                         input2.type='file';
+                         input2.name='arquivo[]';
+
+
+                  var div = document.getElementById('input_files');
+                    div_cont.appendChild(label1);
+                    div_cont.appendChild(input1);
+                    div_cont.appendChild(label2);
+                    div_cont.appendChild(input2);
+
+                     div.appendChild(div_cont);
+
+
+
+                 }
+    </script>
 
 </form>
+
+<hr>
+
+
+<table>
+    <thead>
+    <tr>
+        <th colspan="4">Lista de Pessoas</th>
+    </tr>
+    <tr>
+        <th>Nome:</th>
+        <th>Arquivo:</th>
+        <th>Estado:</th>
+        <th>posicao:</th>
+        <th>Album:</th>
+    </tr>
+    </thead>
+    <tbody>
+    <?php foreach($dados_fotos as $dados): ?>
+        <tr>
+            <td><?= $dados['nome'];?></td>
+            <td><img src="./fotos/<?=$dados['arquivo'];?>" width="200"  height="200"></td>
+            <td><?= $dados['estado'];?></td>
+            <td><?= $dados['posicao'];?></td>
+            <td><?= $dados['fkgaleria'];?></td>
+        </tr>
+    <?php endforeach;?>
+    </tbody>
+</table>
+
+
 
 </body>
 </html>
